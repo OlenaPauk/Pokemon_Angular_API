3e2b1210cb15c66a7e635862fa68d46532ad980c
OlenaPauk <paukoleno4ka.gmail.com>
2020-01-20 16:26:19 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import * as AOS from 'aos';
+import { Component} from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  ngOnInit() {
-    AOS.init();
-    
-  }
+  ngOnInit() { }
 }
+
--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, ViewChild } from '@angular/core';
 import { PokemonsService } from '../shared/pokemons.service';
 import { IPokeList } from '../shared/interfaces/pokemonsList.interface';
 import { IPokeDetails } from '../shared/interfaces/pokeDetails.interface';
@@ -9,52 +9,83 @@ import { IPokeTypeItem } from '../shared/interfaces/pokeTypeItem.interface';
   templateUrl: './pokemons.component.html',
   styleUrls: ['./pokemons.component.css']
 })
-export class PokemonsComponent implements OnInit {  
+export class PokemonsComponent implements OnInit {
   private pagesCount = 0;
 
-  public pokemonsList: IPokeDetails[] = [];  
+  public pokemonsList: IPokeDetails[] = [];
   public pokemonTypesList: IPokeTypeItem[] = [];
   public pokemonDetails: IPokeDetails;
 
   public pokeFilter: string = '';
-    
+
+  public isShowT: boolean=false;
+  public topPosToStartShowing:number = 100;
+  public scrollPosition: number;
+
   constructor(private pokeService: PokemonsService) { }
 
   ngOnInit() {
-    this.LoadPokeList(0); 
-    this.getPokeTypes();   
+    this.LoadPokeList(0);
+    this.getPokeTypes();
+  }
+  @ViewChild("buttonMore", {static: false}) Down: ElementRef;
+  @HostListener('window:scroll')
+  checkScrollTop() {
+    this.scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (this.scrollPosition >= this.topPosToStartShowing) {
+      this.isShowT = true;
+    } else {
+      this.isShowT = false;
+    }
+  }
+
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  gotoDown() {
+    this.Down.nativeElement.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
   }
 
   morePokemons(): void {
     this.pagesCount++;
-    this.LoadPokeList(this.pagesCount);    
+    this.LoadPokeList(this.pagesCount);
   }
 
-  filterPokeType(e){
+  filterPokeType(e) {
     this.pokemonDetails = null;
   }
 
-  pokeDetails(pokeId: number): void{
+  pokeDetails(pokeId: number): void {
     this.pokemonDetails = this.pokemonsList.find(item => item.id === pokeId);
   }
 
   private getPokeTypes(): void {
     this.pokeService.getPokeTypes()
-      .subscribe((poke: IPokeList) => this.pokemonTypesList.push(...poke.results));    
+      .subscribe((poke: IPokeList) => this.pokemonTypesList.push(...poke.results));
   }
 
   private LoadPokeList(page: number): void {
     this.pokeService.getPokeList(page)
-      .subscribe((poke: IPokeList) => {        
-        poke.results.forEach(p => 
+      .subscribe((poke: IPokeList) => {
+        poke.results.forEach(p =>
           // load Poke Details for each Pokemon
           this.pokeService.getPokeDetails(p.url)
-            .subscribe((pokeDetails: IPokeDetails) =>{
+            .subscribe((pokeDetails: IPokeDetails) => {
               this.pokemonsList.push(pokeDetails);
-              this.pokemonsList.sort((u,v)=> u.id-v.id);
+              this.pokemonsList.sort((u, v) => u.id - v.id);
               this.pokeFilter = '';
-            })  
+            })
         );
-      });    
-  } 
+      });
+  }
+
+
 }
